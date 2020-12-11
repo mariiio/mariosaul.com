b71b3051d8d455cce231efb3f7e70d4c8dd22dc3
Mario Saul <mariiosaul@gmail.com>
2020-12-11 15:55:11 +0200

    aboutLink placeholder
    Fixed height for bio to avoid jumping effect
    
    Small fixes

---
+++ src/components/aboutLinks.js
@@ -0,0 +1,9 @@
+import React from "react"
+// import styles from "../styles/AboutLinks.module.css"
+
+export function AboutLinks({ color, children, id, ...props }) {
+  return (
+    <div>
+    </div>
+  )
+}
