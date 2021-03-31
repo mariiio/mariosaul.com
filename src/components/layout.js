6dfec8892159e200ba2babf21df44175179d9673
Mario Saul <mario@imgn.media>
2021-03-31 20:39:52 +0300

    Stick footer at the botton (404 page)

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Header } from './header.js';
-import { Main } from './main.js';
-import { Footer } from './footer.js';
+import React from "react"
+import { Header } from "./header.js"
+import { Main } from "./main.js"
+import { Footer } from "./footer.js"
 
 export function Layout({ children }) {
   return (
-    <div className={'outer'}>
+    <div className={"outer"}>
       <Header />
       <Main>{children}</Main>
       <Footer />
     </div>
-  );
+  )
 }
