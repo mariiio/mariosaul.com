c4a15ce7b32dfe499cbea285981cba551c93198c
Mario Saul <mariiosaul@gmail.com>
2021-03-31 21:23:47 +0300

    SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { SEO } from "../components/seo.js"
 import { Layout } from "../components/layout.js"
 import { Block } from "../components/block.js"
 import { Hero } from "../components/hero.js"
@@ -8,7 +9,19 @@ import { ContactLinks } from "../components/contactLinks.js"
 import "../styles/global.css"
 
 export default function Home() {
+  console.log(
+    "%c HEY THERE!",
+    `
+      background-color: #0058bc;
+      color: white;
+      font-size: 2.8vw;
+      line-height: 1;
+      padding: 4rem 5vw;
+    `
+  )
+
   return [
+    <SEO />,
     <Layout>
       <Block background="clouds">
         <Hero />
