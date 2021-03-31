0958210c616c278bbedaec65cf96ccc44ab8828c
Mario Saul <mariiosaul@gmail.com>
2021-03-31 20:39:52 +0300

    Stick footer at the botton (404 page)

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Layout } from '../components/layout.js';
-import { Block } from '../components/block.js';
-import styles from '../styles/404.module.css';
+import React from "react"
+import { Layout } from "../components/layout.js"
+import { Block } from "../components/block.js"
+import styles from "../styles/404.module.css"
 
 export default () => {
   return [
     <Layout>
-      <Block color="yellow">
+      <Block background="clouds">
         <div className={styles.container}>
           <h1>404 — Page Not Found</h1>
           <p>How did you even get here?</p>
+
           <a href="/">&larr; take me back</a>
         </div>
       </Block>
-    </Layout>
-  ];
-};
+    </Layout>,
+  ]
+}
