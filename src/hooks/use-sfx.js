3bb4ab408d56e69379a12a4fb30535841d3a7024
Mario Saul <mariiosaul@gmail.com>
2021-04-05 12:27:40 +0300

    SettingsProvider to 404

--- src/hooks/use-sfx.js
+++ src/hooks/use-sfx.js
@@ -1,70 +1,57 @@
-import { useSettings } from '../context/settings.js';
-import jump from '../sounds/jump-small.wav'
-import coin from '../sounds/coin.wav'
-import bump from '../sounds/bump.wav'
-import click from '../sounds/click.wav'
+import { useSettings } from "../context/settings.js"
+import jump from "../sounds/jump-small.wav"
+import coin from "../sounds/coin.wav"
+import bump from "../sounds/bump.wav"
+import click from "../sounds/click.wav"
 
-const sfxCache = {};
+const sfxCache = {}
 
 function useSound(sound, { soundEnabled }) {
   if (!soundEnabled) {
-    return [() => {}];
+    return [() => {}]
   }
 
   try {
     if (sfxCache[sound]) {
-      return [() => sfxCache[sound].play()];
+      return [() => sfxCache[sound].play()]
     } else {
-      const sfx = new Audio(sound);
-      sfxCache[sound] = sfx;
+      const sfx = new Audio(sound)
+      sfxCache[sound] = sfx
 
-      return [() => sfxCache[sound].play()];
+      return [() => sfxCache[sound].play()]
     }
   } catch (e) {
-    return [() => {}];
+    return [() => {}]
   }
 }
 
 export function useSfx() {
-  const { soundEnabled } = useSettings();
+  const { soundEnabled } = useSettings()
 
-  const [playBump] = useSound(
-    bump,
-    {
-      soundEnabled,
-      volume: 0.5,
-    },
-  );
+  const [playBump] = useSound(bump, {
+    soundEnabled,
+    volume: 0.5,
+  })
 
-  const [playJump] = useSound(
-    jump,
-    {
-      soundEnabled,
-      volume: 0.5,
-    },
-  );
-  
-  const [playCoin] = useSound(
-    coin,
-    {
-      soundEnabled,
-      volume: 0.5,
-    },
-  );
+  const [playJump] = useSound(jump, {
+    soundEnabled,
+    volume: 0.5,
+  })
 
-  const [playClick] = useSound(
-    click,
-    {
-      soundEnabled,
-      volume: 0.5,
-    },
-  );
+  const [playCoin] = useSound(coin, {
+    soundEnabled,
+    volume: 0.5,
+  })
 
+  const [playClick] = useSound(click, {
+    soundEnabled,
+    volume: 0.5,
+  })
 
   return {
     playJump,
     playClick,
     playBump,
     playCoin,
-  };
+  }
 }
