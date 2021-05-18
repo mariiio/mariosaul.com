dd00e1fa961c3076aa7fa9cf336d5dd6d2707ad5
Mario Saul <mariiosaul@gmail.com>
2021-05-18 18:09:39 +0300

    Options picker component

---
+++ src/components/optionsPicker.js
@@ -0,0 +1,43 @@
+import React from "react"
+import styles from "../styles/optionsPicker.module.css"
+
+export function OptionsPicker({
+  title,
+  selectedOption,
+  options,
+  clickSound = () => {},
+  clickHandler
+}) {
+  const handleClick = event => {
+    event.preventDefault()
+    clickSound()
+    clickHandler(event.target.innerHTML.replace(/\s/, "_").toLowerCase())
+  }
+
+  return (
+    <div className={styles.optionsContainer}>
+      <div className="nes-container with-title is-centered">
+        <p className="title">{title}</p>
+        <div className={styles.options}>
+          {options.map(option => {
+            return (
+              <div key={option} className={styles.option}>
+                <label>
+                  <input
+                    type="radio"
+                    className="nes-radio"
+                    name="difficulty"
+                    checked={selectedOption === option}
+                  />
+                  <span onClick={handleClick}>
+                    {option.replace(/_/, " ").toUpperCase()}
+                  </span>
+                </label>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
