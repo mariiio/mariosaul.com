289c869247c478f8e5453c98be7eedee52f049f7
Mario Saul <mariiosaul@gmail.com>
2021-06-20 18:26:37 +0300

    ⬆️ Go up button

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,50 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { NavBar } from "./navbar.js"
 import { Main } from "./main.js"
 import { Footer } from "./footer.js"
+import { useSfx } from "../hooks/use-sfx.js"
+import { Button } from "./button.js"
+import styles from "../styles/layout.module.css"
 
 export function Layout({ children }) {
+  const { playBump, playClick } = useSfx()
+  const [scrollUp, setScrollUp] = useState(false)
+
+  useEffect(() => {
+    const handleIntersect = (entries, _observer) => {
+      const sentinel = entries[0]
+      setScrollUp(sentinel.boundingClientRect.bottom < 0)
+    }
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 1,
+    }
+    const observer = new IntersectionObserver(handleIntersect, options)
+    observer.observe(document.getElementById("scroll-up-interception-sentinel"))
+  }, [])
+
   return (
     <div className={"outer"}>
       <NavBar />
       <Main>{children}</Main>
       <Footer />
+      <Button
+        className={`nes-btn is-error ${styles.goUpButton}`}
+        style={{ bottom: `${scrollUp ? 20 : -60}px` }}
+        hoverSound={playClick}
+        clickSound={playBump}
+        handleClick={() => {
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+          })
+        }}
+        forceSoundEnabled={true}
+      >
+        <span className={styles.chevron}>&lt;</span>
+      </Button>
     </div>
   )
 }
