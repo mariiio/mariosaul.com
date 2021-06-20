3b617e68c250c8d052a824e52fb8bf4e328a0e72
Mario Saul <mariiosaul@gmail.com>
2021-06-20 19:51:06 +0300

    CV without responsiveness

--- src/components/contactLinks.js
+++ src/components/contactLinks.js
@@ -1,6 +1,9 @@
 import React from "react"
 import luigi from "../images/luigi.png"
+import bowser from "../images/bowser.png"
+import floppy from "../images/floppy.png"
 import castle from "../images/castle.png"
+import cv from "../../static/cv.pdf"
 import styles from "../styles/contactLinks.module.css"
 
 export function ContactLinks({ color, children, id, ...props }) {
@@ -12,62 +15,89 @@ export function ContactLinks({ color, children, id, ...props }) {
             <h2>Connect with Mario!</h2>
 
             <div className={styles.balloons}>
-
-            <a
-              href="https://github.com/mariiio"
-              target="_blank"
-              rel="noreferrer"
-              className={styles.contactLink}
+              <a
+                href="https://github.com/mariiio"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.contactLink}
               >
-              <i className="nes-icon github is-large"></i>
-              <span className={styles.accountName}>/mariiio</span>
-            </a>
-            <a
-              href="https://www.instagram.com/mario_saul"
-              target="_blank"
-              rel="noreferrer"
-              className={styles.contactLink}
+                <i className="nes-icon github is-large"></i>
+                <span className={styles.accountName}>/mariiio</span>
+              </a>
+              <a
+                href="https://www.instagram.com/mario_saul"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.contactLink}
               >
-              <i className="nes-icon instagram is-large"></i>
-              <span className={styles.accountName} href="https://www.instagram.com/mario_saul" target="_blank">
-                @mario_saul
-              </span>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/mario-saul/"
-              target="_blank"
-              rel="noreferrer"
-              className={styles.contactLink}
+                <i className="nes-icon instagram is-large"></i>
+                <span
+                  className={styles.accountName}
+                  href="https://www.instagram.com/mario_saul"
+                  target="_blank"
+                >
+                  @mario_saul
+                </span>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/mario-saul/"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.contactLink}
               >
-              <i className="nes-icon linkedin is-large"></i>
-              <span className={styles.accountName}>/mario-saul</span>
-            </a>
-            <a
-              href="https://medium.com/@mariiosaul"
-              target="_blank"
-              rel="noreferrer"
-              className={styles.contactLink}
+                <i className="nes-icon linkedin is-large"></i>
+                <span className={styles.accountName}>/mario-saul</span>
+              </a>
+              <a
+                href="https://medium.com/@mariiosaul"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.contactLink}
               >
-              <i className="nes-icon medium is-large"></i>
-              <span className={styles.accountName}>@mariiosaul</span>
-            </a>
-            <a
-              href="https://twitter.com/mario_saul"
-              target="_blank"
-              rel="noreferrer"
-              className={styles.contactLink}
+                <i className="nes-icon medium is-large"></i>
+                <span className={styles.accountName}>@mariiosaul</span>
+              </a>
+              <a
+                href="https://twitter.com/mario_saul"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.contactLink}
               >
-              <i className="nes-icon twitter is-large"></i>
-              <span className={styles.accountName}>@mario_saul</span>
-            </a>
+                <i className="nes-icon twitter is-large"></i>
+                <span className={styles.accountName}>@mario_saul</span>
+              </a>
+            </div>
           </div>
-        </div>
         </section>
+        <img loading="lazy" src={luigi} className={styles.luigi} alt="Luigi" />
+      </div>
+
+      <div className={styles.cvContainer}>
+        <section className={styles.balloonContainer}>
+          <div className={"nes-balloon from-right"}>
+            <h2>Open CV</h2>
+            <div>
+              <a
+                href={cv}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  loading="lazy"
+                  src={floppy}
+                  className={styles.cvIcon}
+                  alt="cv"
+                />
+              </a>
+            </div>
+          </div>
+        </section>
+
         <img
           loading="lazy"
-          src={luigi}
-          className={styles.luigi}
-          alt="it's a me! Mario!"
+          src={bowser}
+          className={styles.bowser}
+          alt="Bowser"
         />
       </div>
 
